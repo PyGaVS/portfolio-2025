2f3d261c365ec89c480990f031aee9eefe1e9a60
Lylian <lylian270104@gmail.com>
2025-05-30 14:28:17 +0200

    update exp

--- src/views/Exp.tsx
+++ src/views/Exp.tsx
@@ -71,8 +71,10 @@ function Exp() {
             </a> (1 {t("exp.week")})</p>
           <p className='gray'><a className="blue-link" href="https://www.miditracage-esvia.com/" target="_blank">Esvia
             </a> (3 {t("exp.days")})</p>
-          <p className='gray'><a className="blue-link" href="https://sucre.plus/" target="_blank">Sucre
-            </a> (1 {t("exp.day")})</p>  
+          <p className='gray'><a className="blue-link" href="https://sucre.plus/" target="_blank">Sucre+
+            </a> (1 {t("exp.day")})</p>
+          <p className='gray'><a className="blue-link" href="https://amb-marbrerie.com/" target="_blank"> Atelier Marbreri Bremand
+            </a> (1 {t("exp.month")})</p>
         </div>
       </details>
     </>
